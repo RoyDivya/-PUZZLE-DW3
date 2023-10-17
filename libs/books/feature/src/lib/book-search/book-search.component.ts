2bd54ab02930a95251663edca35ead3e2ef5a484
Divya Roy <divya.roy@infosys.com>
2023-10-17 20:04:27 +0530

    Task 2: Adding instant search

--- libs/books/feature/src/lib/book-search/book-search.component.ts
+++ libs/books/feature/src/lib/book-search/book-search.component.ts
@@ -1,6 +1,7 @@
-import { Component} from '@angular/core';
+import { Component, OnInit, OnDestroy} from '@angular/core';
 import { Store } from '@ngrx/store';
 import {
+  ReadingListBook,
   addToReadingList,
   clearSearch,
   getAllBooks,
@@ -8,19 +9,22 @@ import {
 } from '@tmo/books/data-access';
 import { FormBuilder } from '@angular/forms';
 import { Book } from '@tmo/shared/models';
+import { debounceTime, distinctUntilChanged, takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'tmo-book-search',
   templateUrl: './book-search.component.html',
   styleUrls: ['./book-search.component.scss']
 })
-export class BookSearchComponent  {
+export class BookSearchComponent implements OnInit, OnDestroy {
+  books: ReadingListBook[];
   bookList$=this.store.select(getAllBooks);
-
   searchForm = this.form.group({
     term: ''
   });
 
+  unSubscribeSubject$ = new Subject();
   constructor(
     private readonly store: Store,
     private readonly form: FormBuilder
@@ -29,6 +33,22 @@ export class BookSearchComponent  {
   get searchTerm(): string {
     return this.searchForm.value.term;
   }
+
+  ngOnInit(): void {
+    this.searchForm.controls['term'].valueChanges.pipe(
+      debounceTime(500),
+      distinctUntilChanged(),
+      takeUntil(this.unSubscribeSubject$))
+      .subscribe(() => this.searchBooks(),
+        err => console.error(err, 'Something went wrong in searching books')
+      );
+  }
+
+  ngOnDestroy(): any {
+    this.unSubscribeSubject$.next();
+    this.unSubscribeSubject$.complete();
+  }
+
 
   addBookToReadingList(book: Book) {
     this.store.dispatch(addToReadingList({ book }));
