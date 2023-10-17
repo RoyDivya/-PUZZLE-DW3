582539a7f88c3b3223d4fc6c8735edbd456c762a
Divya Roy <divya.roy@infosys.com>
2023-10-17 22:16:16 +0530

    Task 3: Undo add and remove from reading list

--- apps/okreads-e2e/src/integration/reading-list.spec.ts
+++ apps/okreads-e2e/src/integration/reading-list.spec.ts
@@ -11,4 +11,45 @@ describe('When: I use the reading list feature', () => {
       'My Reading List'
     );
   });
+
+  it('Then: I should be able to add an item to the list and then undo', () => {
+    cy.searchBooks('javascript');
+
+    // required to cover the edge case scenario when reading list is empty
+    cy.addToReadingList();
+    // initial count of reading list item
+    cy.readingListItem().then(initialItemCount => {
+      cy.addToReadingList();
+
+      cy.undoAction();
+
+      // final count of reading list
+      cy.readingListItem().then(finalItemCount => {
+        expect(initialItemCount).to.equal(finalItemCount);
+      });
+    });
+  });
+
+
+  it('Then:  I should be able to remove an item from the list and then undo', () => {
+    cy.searchBooks('javascript');
+
+    cy.addToReadingList();
+
+    // open reading list
+    cy.get('[data-testing="toggle-reading-list"]').click();
+
+    // initial count of reading list item
+    cy.readingListItem().then(initialItemCount => {
+
+      cy.removeFromReadingList();
+
+      cy.undoAction();
+
+      // final count of reading list item
+      cy.readingListItem().then(finalItemCount => {
+        expect(initialItemCount).to.equal(finalItemCount);
+      });
+    });
+  });
 });
--- libs/books/data-access/src/lib/+state/reading-list.effects.spec.ts
+++ libs/books/data-access/src/lib/+state/reading-list.effects.spec.ts
@@ -1,23 +1,39 @@
-import { TestBed } from '@angular/core/testing';
+import { fakeAsync, TestBed } from '@angular/core/testing';
 import { ReplaySubject } from 'rxjs';
 import { provideMockActions } from '@ngrx/effects/testing';
-import { provideMockStore } from '@ngrx/store/testing';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { HttpTestingController } from '@angular/common/http/testing';
 
-import { SharedTestingModule } from '@tmo/shared/testing';
+import { createBook, createReadingListItem, SharedTestingModule } from '@tmo/shared/testing';
 import { ReadingListEffects } from './reading-list.effects';
 import * as ReadingListActions from './reading-list.actions';
+import { MatSnackBar, MatSnackBarModule, MatSnackBarRef } from '@angular/material/snack-bar';
+import { Book, ReadingListItem } from '@tmo/shared/models';
+
 
 describe('ToReadEffects', () => {
   let actions: ReplaySubject<any>;
   let effects: ReadingListEffects;
   let httpMock: HttpTestingController;
+  let item: ReadingListItem;
+  let book: Book;
+  let snackbar: MatSnackBar;
+  let store : MockStore;
+
+  const bookItem = createReadingListItem('A');
+  const itemBook = createBook('B');
+
+  beforeAll(() => {
+    item = createReadingListItem('A');      
+    book = createBook('A');
+  })
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [SharedTestingModule],
+      imports: [SharedTestingModule, MatSnackBarModule],
       providers: [
         ReadingListEffects,
+        { provide: MatSnackBar, useVale: {open: (param1, param2) => { return; }}},
         provideMockActions(() => actions),
         provideMockStore()
       ]
@@ -25,11 +41,13 @@ describe('ToReadEffects', () => {
 
     effects = TestBed.inject(ReadingListEffects);
     httpMock = TestBed.inject(HttpTestingController);
+    actions = new ReplaySubject();
+    store = TestBed.inject(MockStore);
+    snackbar = TestBed.inject(MatSnackBar);
   });
 
   describe('loadReadingList$', () => {
-    it('should work', done => {
-      actions = new ReplaySubject();
+    it('should fetch reading list successfully', done => {
       actions.next(ReadingListActions.init());
 
       effects.loadReadingList$.subscribe(action => {
@@ -42,4 +60,92 @@ describe('ToReadEffects', () => {
       httpMock.expectOne('/api/reading-list').flush([]);
     });
   });
+  
+  describe('addBook$', () => {
+    it('should add a book to the reading list successfully', fakeAsync(() => {
+      actions.next(ReadingListActions.addToReadingList({ book, add: false  }));
+
+      effects.addBook$.subscribe(action => {
+        expect(action).toEqual(
+          ReadingListActions.confirmedAddToReadingList({ book, add: false })
+        );
+      })
+
+      httpMock.expectOne('/api/reading-list').flush({});
+    }));
+
+
+    it('should undo the added book when API returns error', fakeAsync(() => {
+      actions.next(ReadingListActions.addToReadingList({ book, add: false}));
+
+      effects.addBook$.subscribe(action => {
+        expect(action).toEqual(
+          ReadingListActions.failedAddToReadingList({ book })
+        );
+      });
+
+      httpMock.expectOne('/api/reading-list').flush({}, { status: 500, statusText: 'server error' });
+
+    }));
+
+    it('show snackbar on add book', async() => {
+      actions = new ReplaySubject();
+      actions.next(ReadingListActions.confirmedAddToReadingList({ book, add:false }));
+
+      effects.showSnackBarOnAdd$.subscribe(() => {
+        snackbar.open(`Added ${book.title} to reading list`,'Undo', { duration: 2000}).onAction().subscribe((action)=>{
+          expect(action).toEqual(
+            ReadingListActions.removeFromReadingList({ item:bookItem, remove:true })
+        )
+        })
+      });
+    });
+
+  });  
+
+  describe('removeBook$', () => {
+    it('should remove book successfully from reading list', done => {
+      actions.next(ReadingListActions.removeFromReadingList({ item, remove: false }));
+
+      effects.removeBook$.subscribe(action => {
+        expect(action).toEqual(
+          ReadingListActions.confirmedRemoveFromReadingList({ item, remove: false  })
+        );
+
+        done();
+      });
+
+      httpMock.expectOne(`/api/reading-list/${item.bookId}`).flush({});
+    });
+
+
+    it('should undo removed book when API returns error', fakeAsync(() => {
+      actions.next(ReadingListActions.removeFromReadingList({ item, remove: false  }));
+
+      effects.removeBook$.subscribe(action => {
+        expect(action).toEqual(
+          ReadingListActions.failedRemoveFromReadingList({ item })
+        );
+      });
+
+      httpMock.expectOne(`/api/reading-list/${item.bookId}`).flush({}, { status: 500, statusText: 'server error' });
+    }));
+
+    it('show snackbar on remove book', async() => {
+      actions = new ReplaySubject();
+      actions.next(ReadingListActions.confirmedRemoveFromReadingList({ item, remove:false }));
+
+      effects.showSnackBarOnRemove$.subscribe(() => {
+        snackbar.open(`Removed ${item.title} to reading list`,'Undo', { duration: 2000}).onAction().subscribe((action)=>{
+          expect(action).toEqual(
+            ReadingListActions.addToReadingList({ book:itemBook, add:true })
+          );
+        })
+      });
+    });
+
+  });
+
 });
+
+
